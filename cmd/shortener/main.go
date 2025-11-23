dfc1f923059520f6675584d7030d0b2f516b9527
Robert Sabirov <rasabirov@avito.ru>
2025-11-23 16:26:43 +0300

    refactor: move configuration into config package

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"database/sql"
-	"flag"
 	"net/http"
 	"os"
 
 	"github.com/go-chi/chi/v5"
 	_ "github.com/jackc/pgx/v5/stdlib"
-	"github.com/pressly/goose/v3"
 	"github.com/rs/zerolog"
 
 	_ "github.com/hydra13/shortify"
+	"github.com/hydra13/shortify/internal/config"
+	dbConfig "github.com/hydra13/shortify/internal/config/db"
 	longUrlHandler "github.com/hydra13/shortify/internal/handlers/get_long_url_handler"
 	shortUrlByJsonHandler "github.com/hydra13/shortify/internal/handlers/get_short_url_by_json_handler"
 	shortUrlHandler "github.com/hydra13/shortify/internal/handlers/get_short_url_handler"
@@ -19,38 +19,19 @@ import (
 	ping "github.com/hydra13/shortify/internal/handlers/ping_handler"
 	"github.com/hydra13/shortify/internal/middlewares/compresser"
 	"github.com/hydra13/shortify/internal/middlewares/logger"
-	repository "github.com/hydra13/shortify/internal/repositories"
-	dbStorage "github.com/hydra13/shortify/internal/repositories/db_storage"
-	fileStorage "github.com/hydra13/shortify/internal/repositories/file_storage"
-	inmemorydb "github.com/hydra13/shortify/internal/repositories/inmemory_db"
 	gen "github.com/hydra13/shortify/internal/services/short_id_generator"
 	shorter "github.com/hydra13/shortify/internal/services/shorter"
 	validator "github.com/hydra13/shortify/internal/services/url_validator"
 	urlsKeeper "github.com/hydra13/shortify/internal/services/urls_keeper"
 )
 
-type Mode int
-
-const (
-	ModeInMemory Mode = iota + 1
-	ModeFileStorage
-	ModeDBStorage
-)
-
-var (
-	serverAddr      string
-	baseURL         string = "http://localhost:8080"
-	fileStoragePath string = "./storage.json"
-	dbDSN           string = "postgresql://postgres:postgres@localhost:5432/shortify_data?sslmode=disable"
-	dbDriver        string = "pgx"
-	currentMode     Mode   = ModeInMemory
-)
-
 func main() {
-	parseConfigs()
 	log := zerolog.New(os.Stdout).With().Timestamp().Logger()
 
-	dbInstance, err := sql.Open(dbDriver, dbDSN)
+	conf := config.NewConfig()
+	conf.ParseConfig()
+
+	dbInstance, err := sql.Open(conf.DatabaseDriver, conf.DatabaseDSN)
 	if err != nil {
 		log.Error().Err(err).Msg("failed to connect to db")
 	}
@@ -60,7 +41,7 @@ func main() {
 		}
 	}()
 
-	repo, err := getRepository(dbInstance, log)
+	repo, err := dbConfig.GetRepository(dbInstance, conf, log)
 	if err != nil {
 		log.Fatal().Err(err).Msg("failed to init repo")
 	}
@@ -68,12 +49,12 @@ func main() {
 	generator := gen.New()
 	urlValidator := validator.New()
 	uk := urlsKeeper.New(repo)
-	s := shorter.New(urlValidator, uk, generator, baseURL, log)
+	s := shorter.New(urlValidator, uk, generator, conf.BaseURL, log)
 
 	getLongURLHandler := longUrlHandler.CreateHandler(uk, log)
-	getShortURLHandler := shortUrlHandler.CreateHandler(s, baseURL, log)
-	getShortURLbyJSONHandler := shortUrlByJsonHandler.CreateHandler(s, baseURL, log)
-	getShortURLSBatchHandler := shortUrlsBatchHandler.CreateHandler(s, baseURL, log)
+	getShortURLHandler := shortUrlHandler.CreateHandler(s, conf.BaseURL, log)
+	getShortURLbyJSONHandler := shortUrlByJsonHandler.CreateHandler(s, conf.BaseURL, log)
+	getShortURLSBatchHandler := shortUrlsBatchHandler.CreateHandler(s, conf.BaseURL, log)
 
 	r := chi.NewRouter()
 
@@ -92,109 +73,5 @@ func main() {
 		r.Post("/batch", getShortURLSBatchHandler)
 	})
 
-	http.ListenAndServe(serverAddr, r)
-}
-
-func parseConfigs() {
-	flag.StringVar(&serverAddr, "a", ":8080", "server address")
-	flag.Func("f", "file storage path (default: \"./storage.json\")", func(path string) error {
-		if len(path) == 0 {
-			return nil
-		}
-
-		fileStoragePath = path
-
-		if currentMode <= ModeFileStorage {
-			currentMode = ModeFileStorage
-		}
-
-		return nil
-	})
-	flag.Func("b", "result base url (default: \"http://localhost:8080\")", func(url string) error {
-		if len(url) == 0 {
-			return nil
-		}
-
-		baseURL = url
-
-		if baseURL[len(baseURL)-1] == '/' {
-			baseURL = baseURL[:len(baseURL)-1]
-		}
-
-		return nil
-	})
-	flag.Func("d", "database DSN (default: \"postgresql://postgres:postgres@localhost:5432/shortify_data?sslmode=disable\")", func(dsn string) error {
-		if len(dsn) == 0 {
-			return nil
-		}
-
-		dbDSN = dsn
-
-		if currentMode <= ModeDBStorage {
-			currentMode = ModeDBStorage
-		}
-
-		return nil
-	})
-
-	flag.Parse()
-
-	if addr, ok := os.LookupEnv("SERVER_ADDRESS"); ok {
-		serverAddr = addr
-	}
-
-	if url, ok := os.LookupEnv("BASE_URL"); ok {
-		baseURL = url
-	}
-
-	if filePath, ok := os.LookupEnv("FILE_STORAGE_PATH"); ok {
-		fileStoragePath = filePath
-
-		if currentMode <= ModeFileStorage {
-			currentMode = ModeFileStorage
-		}
-	}
-
-	if dsn, ok := os.LookupEnv("DATABASE_DSN"); ok {
-		dbDSN = dsn
-
-		if currentMode <= ModeDBStorage {
-			currentMode = ModeDBStorage
-		}
-	}
-}
-
-func getRepository(dbInstance *sql.DB, log zerolog.Logger) (repository.Repository, error) {
-	switch currentMode {
-	case ModeDBStorage:
-		err := applyMigrations(dbInstance)
-		if err != nil {
-			return nil, err
-		}
-
-		repo, err := dbStorage.New(dbInstance, log)
-		if err != nil {
-			return nil, err
-		}
-
-		return repo, nil
-	case ModeFileStorage:
-		repo, err := fileStorage.New(fileStoragePath, log)
-		if err != nil {
-			return nil, err
-		}
-
-		return repo, nil
-	default:
-	}
-
-	return inmemorydb.New(), nil
-}
-
-func applyMigrations(db *sql.DB) error {
-	if err := goose.SetDialect(dbDriver); err != nil {
-		return err
-	}
-
-	return goose.Up(db, "migrations")
+	log.Fatal().Err(http.ListenAndServe(conf.ServerAddr, r)).Msg("exit")
 }
---
+++ internal/config/config.go
@@ -0,0 +1,102 @@
+package config
+
+import (
+	"flag"
+	"os"
+)
+
+type Mode int
+
+const (
+	ModeInMemory Mode = iota + 1
+	ModeFileStorage
+	ModeDBStorage
+)
+
+type Config struct {
+	ServerAddr      string
+	BaseURL         string
+	FileStoragePath string
+	DatabaseDSN     string
+	DatabaseDriver  string
+	CurrentMode     Mode
+}
+
+func NewConfig() *Config {
+	return &Config{
+		BaseURL:         "http://localhost:8080",
+		FileStoragePath: "./storage.json",
+		DatabaseDSN:     "postgresql://postgres:postgres@localhost:5432/shortify_data?sslmode=disable",
+		DatabaseDriver:  "pgx",
+		CurrentMode:     ModeInMemory,
+	}
+}
+
+func (c *Config) ParseConfig() {
+	flag.StringVar(&c.ServerAddr, "a", ":8080", "server address")
+	flag.Func("f", "file storage path (default: \"./storage.json\")", func(path string) error {
+		if len(path) == 0 {
+			return nil
+		}
+
+		c.FileStoragePath = path
+
+		if c.CurrentMode <= ModeFileStorage {
+			c.CurrentMode = ModeFileStorage
+		}
+
+		return nil
+	})
+	flag.Func("b", "result base url (default: \"http://localhost:8080\")", func(url string) error {
+		if len(url) == 0 {
+			return nil
+		}
+
+		if url[len(url)-1] == '/' {
+			url = url[:len(url)-1]
+		}
+
+		c.BaseURL = url
+
+		return nil
+	})
+	flag.Func("d", "database DSN (default: \"postgresql://postgres:postgres@localhost:5432/shortify_data?sslmode=disable\")", func(dsn string) error {
+		if len(dsn) == 0 {
+			return nil
+		}
+
+		c.DatabaseDSN = dsn
+
+		if c.CurrentMode <= ModeDBStorage {
+			c.CurrentMode = ModeDBStorage
+		}
+
+		return nil
+	})
+
+	flag.Parse()
+
+	if addr, ok := os.LookupEnv("SERVER_ADDRESS"); ok {
+		c.ServerAddr = addr
+	}
+
+	if url, ok := os.LookupEnv("BASE_URL"); ok {
+		c.BaseURL = url
+	}
+
+	if filePath, ok := os.LookupEnv("FILE_STORAGE_PATH"); ok {
+		c.FileStoragePath = filePath
+
+		if c.CurrentMode <= ModeFileStorage {
+			c.CurrentMode = ModeFileStorage
+		}
+	}
+
+	if dsn, ok := os.LookupEnv("DATABASE_DSN"); ok {
+		c.DatabaseDSN = dsn
+
+		if c.CurrentMode <= ModeDBStorage {
+			c.CurrentMode = ModeDBStorage
+		}
+	}
+}
